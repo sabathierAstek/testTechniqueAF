d7f8971bde3b89db9d5b737fb1e1f557a6b6829b
Sabathier <dsabathier@groupeastek.fr>
2024-06-12 11:24:30 +0200

    feat(logging): update logging aspect to record processing time

--- demo/src/main/java/com/example/demo/LoggingAspect.java
+++ demo/src/main/java/com/example/demo/LoggingAspect.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,14 +14,19 @@ public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* com.example.demo.UserController.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
+    @Around("execution(* com.example.demo.UserController.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
         logger.info("Entering in Method :  " + joinPoint.getSignature().getName());
         logger.info("Arguments :  " + joinPoint.getArgs());
-    }
-
-    @AfterReturning(pointcut = "execution(* com.example.demo.UserController.*(..))", returning = "result")
-    public void logAfter(JoinPoint joinPoint, Object result) {
+        
+        Object result = joinPoint.proceed();
+        
+        long elapsedTime = System.currentTimeMillis() - start;
+        logger.info("Exiting from Method : " + joinPoint.getSignature().getName());
         logger.info("Method Return value : " + result);
+        logger.info("Method execution time: " + elapsedTime + " milliseconds.");
+        
+        return result;
     }
 }
